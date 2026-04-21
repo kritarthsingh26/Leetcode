1e3efeac382b9a41e4c2a940565eb89b90f3bba0
kritarthsingh26 <kritarthsingh2609@gmail.com>
2026-04-21 21:32:34 +0530

    Time: 159 ms (51.31%), Space: 153.2 MB (64.92%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> parent;
+
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa != pb) parent[pa] = pb;
+    }
+
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        parent.resize(n);
+
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+       
+        for (auto &v : allowedSwaps) {
+            unite(v[0], v[1]);
+        }
+
+      
+        unordered_map<int, vector<int>> groups;
+        for (int i = 0; i < n; i++) {
+            groups[find(i)].push_back(i);
+        }
+
+        int ans = 0;
+
+        
+        for (auto &g : groups) {
+            unordered_map<int, int> freq;
+
+            
+            for (int idx : g.second) {
+                freq[source[idx]]++;
+            }
+
+           
+            for (int idx : g.second) {
+                if (freq[target[idx]] > 0) {
+                    freq[target[idx]]--;
+                } else {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
